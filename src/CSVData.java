c4bb77ada67d66b549d409f681625ec0c4bc5708
erin <erin@Bettys-MacBook-Pro.local>
2016-10-12 11:15:31 -0700

    Add get and set methods for the array

--- src/CSVData.java
+++ src/CSVData.java
@@ -22,5 +22,85 @@ public class CSVData {
 	public static CSVData readCSVFile (String fileName, int numLinesToIgnore){
 		return null;
 	}
+	
+	/***
+	 * Get row index
+	 * @param rowIndex
+	 * @return row index
+	 */
+	public double[] getRow(int rowIndex) {
+		return null;
+	}
+	
+	/***
+	 * Get column name
+	 * @param ColumnName
+	 * @return column name
+	 */
+	public double[] getColumn (String ColumnName){
+		return null;
+	}
+	
+	/***
+	 * get multiple rows out of the array
+	 * @param startingRowIndex
+	 * @param endingRowIndex
+	 * @return Values within multiple rows
+	 */
+	public double[][] getMultipleRows ( int startingRowIndex, int endingRowIndex){
+		return null;
+	}
+	
+//	public double [][] getRows (int[] rowIndexes ){
+//		return null;
+//	}
+	
+	/***
+	 * get multiple columns
+	 * @param startingColumnIndex
+	 * @param endingRowIndex
+	 * @return return values within multiple columns
+	 */
+	public double [][] getMultipleColumns (int startingColumnIndex, int endingRowIndex){
+		return null;
+	}
+	
+//	public double [][] getColumns (int[] rowIndexes ){
+//	return null;
+//}
+	
+//	public double [][] getColumns (String[] colNames ){
+//	return null;
+//}
+	
+	/*** 
+	 * Sets new values within specific values within the array
+	 * @param rowIndex
+	 * @param columnIndex
+	 * @param newValue
+	 */
+	public void setNewValues (int rowIndex, int columnIndex, double newValue) {
+		
+	}
+	
+	public void setNewValues (int rowIndex, double newValue){
+		
+	}
+	
+	public void setNewValues (String columnName, double newValue){
+
+	}
+	
+	public double [] getColumnNames (String[] ColumnNames){
+		return null;
+	}
+	
+	public void saveToFile (String FileName){
+		
+	}
+	
+	
+	
+	
 
 }
